2741546b4187b8fb43ae6bccff7ae298e0475f50
Romeu Gomes de Sousa <romeu.gomes@alfanetworks.com.br>
2026-05-06 08:35:46 -0300

    estrutura básica para entendimento do funcionamento

--- src/EmitirNfse.php
+++ src/EmitirNfse.php
@@ -4,6 +4,25 @@ namespace romeugomesdev\GovbrNfse;
 
 class EmitirNfse
 {
+    private $xml;
+    private $municipio;
+    private $municipios = array(
+        '3304557' => array(
+            'uf_ibge'=>33,
+            'uf'=>'RJ',
+            'nome'=>'Rio de Janeiro'
+        )
+        );
+
+    public function __construct ($xml, $municipio)
+    {
+        $this->xml = $xml;
+        if(!in_array($municipio,array_keys($municipios),false)) {
+            throw new Exception("Municipio não atendido");
+        }
+        $this->municipio = $municipio;
+    }
+
     public function gerar()
     {
         return "NFSe processada com sucesso!";
